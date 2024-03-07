c882799f859264ffa3821a6fb81ebae39ecc9536
gn <115848472+g-nbo@users.noreply.github.com>
2024-03-07 13:15:14 -0500

    Add light-gray background, Add Button that initiates user prompt Add RBG tiles

---
+++ js.js
@@ -0,0 +1,64 @@
+const container = document.querySelector('#container');
+
+const btn = document.querySelector('#btn');
+
+function createBoxes() {
+    let numBoxes = prompt('number of squares per side: ', 20);
+
+    if(numBoxes <= 100) {
+        for(i = 0; i < numBoxes; i++) {
+        
+
+            for(j = 0; j < numBoxes; j++) {
+                const boxes = document.createElement('div');
+                boxes.classList.add('boxes');
+                boxes.style.setProperty('height', `${Math.floor(1 / numBoxes * 100)}%`);
+                boxes.style.setProperty("width", `${Math.floor(1 / numBoxes * 100)}%`);
+                container.appendChild(boxes);
+                
+    
+                boxes.addEventListener(
+                    "mouseenter",
+                    (event) => {
+                        event.target.style.backgroundColor = getRandColor();
+                
+                        setTimeout(() => {
+                            event.target.style.backgroundColor = "";
+                        }, 600);
+                    },
+                    false,
+                );
+            }
+    
+            
+        }
+    }
+    else {
+        alert('Please enter a number 100 or less!');
+    }
+    }
+
+    
+
+function clearBoard() {
+    let board = document.getElementById('container');
+    while (board.hasChildNodes()){
+        board.removeChild(board.firstChild);
+    }
+}
+
+function getRandColor() {
+    let colors = ["red", "orange", "yellow", 'green', 'blue', 'indigo', 'purple'];
+
+    let randColor = colors[Math.floor(Math.random() * colors.length)];
+    
+    return randColor;
+}
+
+btn.addEventListener('click', () => {
+    clearBoard();
+    createBoxes();
+    
+})
+
+
